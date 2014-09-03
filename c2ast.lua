40d816d4db59b7e2a6429a8db7687e15d392dc93
Moligaloo <moligaloo@gmail.com>
2014-09-03 16:02:11 +0800

    support decimal, octal and hexdecimal integers

--- c2ast.lua
+++ c2ast.lua
@@ -8,6 +8,12 @@ local json = require 'dkjson'
 local defs = {
 	simplify_type = function(s)
 		return s:gsub("^%s*(.-)%s*$", "%1"):gsub("%s+", ' ')
+	end,
+
+	tonumber = tonumber,
+	
+	tonumber8 = function(s) 
+		return tonumber(s, 8)
 	end
 }
 
@@ -21,16 +27,31 @@ local grammar_string = [[
 		@func_def@
 		<<<
 		{:return_type:type:} S {:name:identifier:} S0 
-		'(' {:params: {| params |} :} ')' 
+		'(' {:params: {| params |} :} ')' S0
+		'{' S0 {:code:code:} S0 '}'
 		>>>
 	|}
 	identifier <- [a-zA-Z_][a-zA-Z0-9_]*
 	params <- (param_def (S0 ',' S0 param_def)* )?
 	param_def <- {| {:type:param_type:} S0 {:name:identifier:} |} 
 	param_type <- {'const'? S0 type S0 ('*'+ 'const'?  / '') &[_a-zA-Z ] } -> simplify_type
+	code <- {| (S0 statement S0) + |}
+	statement <- expression_stmt 
+	expression_stmt <- 
+		{| {:expression:expression:} S0 ';' |}
+	expression <- {| @integer@ {:value:integer:} |}
 
 	S <- %s+
 	S0 <- %s*
+
+	nzdigit <- [1-9]
+	decimal <- ('0' / nzdigit %d*) !%d
+	hexdigit <- ('0x' / '0X') %x+
+	octal <- '0' [0-7]+
+	integer <-  
+		{hexdigit} -> tonumber 
+		/ {octal} -> tonumber8 
+		/ {decimal} -> tonumber
 ]]
 
 grammar_string = grammar_string
