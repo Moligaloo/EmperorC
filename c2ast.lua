083f2c5c35ec134a6cf493115e201d444637d1af
Moligaloo <moligaloo@gmail.com>
2014-09-04 17:38:14 +0800

    support #pattern# replacement of grammar

--- c2ast.lua
+++ c2ast.lua
@@ -34,33 +34,33 @@ local defs = {
 local grammar_string = [[
 	units <- {| (S0 unit S0)+ |}
  	unit <- include_directive / func_def
-	include_directive <- {| @include@ <<< '#include' S0 '<' {:filename: filename :} '>' >>> |} 
+	include_directive <- {| @include@ <<< '#include' S0 '<' #filename# '>' >>> |} 
 	filename <- {[^"<>]+}
 	func_def <- {|
 		@func_def@
 		<<<
 		{:return_type:type:} {:name:identifier:} S0 
 		'(' {:params: {| params |} :} ')' S0
-		'{' S0 {:code:code:} S0 '}'
+		'{' S0 #code# S0 '}'
 		>>>
 	|}
 	identifier <- [a-zA-Z_][a-zA-Z0-9_]*
 	params <- (param_def (S0 ',' S0 param_def)* )?
-	param_def <- {| {:type:type:}{:name:identifier:} |} 
+	param_def <- {| #type# {:name:identifier:} |} 
 	type_prefix <- 'const'? S0 identifier
 	type_suffix <- ('*'+ 'const'?)? &[_a-zA-Z ]
 	type <- {type_prefix S0 type_suffix} -> simplify_type
 	code <- {| (S0 statement S0) + |}
 	statement <- return_stmt / expression_stmt / var_decl_stmt
 	expression_stmt <- 
-		{| {:expression:expression:} S0 ';' |}
+		{| #expression# S0 ';' |}
 	expression <-
 		{| @float@ {:value:float:} |} 
 		/ {| @integer@ {:value:integer:} |}
 		/ {| @string@ {:value:string:} |}
 		/ {| @func_call@ {:value:func_call:} |}
 	return_stmt <- 
-		{| @return@ <<< 'return' S0 {:expression:expression:} S0 ';' >>> |}
+		{| @return@ <<< 'return' S0 #expression# S0 ';' >>> |}
 
 	var_decl_stmt <- 
 		{| @var_decl@ <<< var_decl S0 ';' >>> |}
@@ -76,7 +76,7 @@ local grammar_string = [[
 	S0 <- %s*
 
 	func_call <- 
-		{| {:name:identifier:} S0 '(' S0 {:arguments:arguments:} S0 ')' |}
+		{| {:name:identifier:} S0 '(' S0 #arguments# S0 ')' |}
 
 	arguments <- {| (expression (S0 ',' S0 expression)*)? |}
 
@@ -99,10 +99,9 @@ grammar_string = grammar_string
 	:gsub("<<<", "{:start_offset: {} :}" )
 	:gsub(">>>", "{:end_offset: {} :}")
 	:gsub("@([%w_]+)@", "{:type: '' -> '%1' :}")
+	:gsub('#([%w_]+)#', '{:%1:%1:}')
 
 local grammar = re.compile(grammar_string, defs)
-
--- print(json.encode(grammar:match("int a,b,c;")))
 
 function c2ast(source)
 	local fp = io.open(source, "r")
