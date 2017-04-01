d5ba63dfd6b404a3f4589340bcfed48cfa7720a1
Moligaloo <moligaloo@gmail.com>
2017-04-01 10:19:15 +0800

    add testing code for decompiling

--- test/test.lua
+++ test/test.lua
@@ -10,13 +10,23 @@ function io_read(filename)
 	return content
 end
 
-for _, file in ipairs{'hello', 'globals', 'while', 'expression', 'selection'} do
-	it("Testing file " .. file, function() 
+local filenames = {'hello', 'globals', 'while', 'expression', 'selection'}
+
+for _, file in ipairs(filenames) do
+	it("Testing file compiling " .. file, function() 
 		local c_source = ("test/c/%s.c"):format(file)
 		local json_source = io_read(("test/ast/%s.json"):format(file))
 		local session = emperor.session.new()
 		session:load_c(c_source)
 
 		assert.same(session.definitions, json.decode(json_source))
+	end)
+end
+
+for _, file in ipairs(filenames) do
+	it("Testing file decompiling " .. file, function()
+		local session = emperor.session.new()
+		session:load_ast(("test/ast/%s.json"):format(file))
+		session:to_c()
 	end)
 end
